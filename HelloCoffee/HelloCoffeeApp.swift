9931b789e939360b377836c36e9fc5b8e1eea5ed
DJDrama <putzzang@naver.com>
2024-11-21 16:16:20 +0900

    Implementing Webservice and Order Model

---
+++ HelloCoffee/HelloCoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloCoffeeApp.swift
+//  HelloCoffee
+//
+//  Created by Dongjun Lee on 11/21/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloCoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
